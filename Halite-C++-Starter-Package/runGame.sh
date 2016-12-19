c1bd412f6ae674dbd280295791f9f70b74934142
J.A. van Egmond <j.a.vanegmond@tudelft.nl>
2016-12-19 16:28:08 +0100

    Added touching game.log to create it

--- Halite-C++-Starter-Package/runGame.sh
+++ Halite-C++-Starter-Package/runGame.sh
@@ -3,6 +3,7 @@
 mkdir -p replays
 mv *.hlt replays/
 rm game.log
+touch game.log
 g++ -std=c++11 MyBot.cpp -o MyBot.o
 g++ -std=c++11 RandomBot.cpp -o RandomBot.o
 ./halite -d "30 30" "./MyBot.o" "./RandomBot.o"
