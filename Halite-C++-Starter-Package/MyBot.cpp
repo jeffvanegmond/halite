ac61b1af033d92b3e34b536946874efa50fe0883
J.A. van Egmond <j.a.vanegmond@tudelft.nl>
2016-12-20 09:02:20 +0100

    Reverted starter package to original state

--- Halite-C++-Starter-Package/MyBot.cpp
+++ Halite-C++-Starter-Package/MyBot.cpp
@@ -9,94 +9,8 @@
 #include "hlt.hpp"
 #include "networking.hpp"
 
-namespace h_log {
-	std::fstream cout;
-}
-
-bool hasEnemies(const hlt::Location& location, hlt::GameMap& map) {
-	unsigned char owner = map.getSite(location).owner;
-	for(unsigned char dir = 1; dir <= 5; ++dir) {
-		if(map.getSite(location, dir).owner != owner)
-			return true;
-	}
-	return false;
-}
-
-unsigned char findShortestPathToEnemy(const hlt::Location& location, hlt::GameMap& map) {
-	int shortest_steps = std::max(map.height, map.width);
-	unsigned char strategy = 0;
-	unsigned char owner = map.getSite(location).owner;
-	//for(unsigned char dir = 1; dir <=5; ++dir) {
-	//	int steps = 0;
-	//	hlt::Location curr_loc = location;
-	//	do{
-	//		++steps;
-	//		curr_loc = map.getLocation(curr_loc, dir);
-	//	} while(map.getSite(curr_loc).owner == owner && steps < shortest_steps);
-	//	if(steps < shortest_steps) {
-	//		shortest_steps = steps;
-	//		strategy = dir;
-	//	}
-	//}
-	//return strategy;
-	float shortest_distance = float(shortest_steps);
-	float angle;
-	for(unsigned short x = 0; x < map.width; ++x) {
-		for(unsigned short y = 0; y < map.height; ++y) {
-			if(map.getSite({x, y}).owner == owner)
-				continue;
-			float distance = map.getDistance(location, {x, y});
-			if(distance < shortest_distance) {
-				shortest_distance = distance;
-				angle = map.getAngle(location, {x, y});
-				h_log::cout << "Angle update: " << angle / M_PI << std::endl;
-			}
-		}
-	}
-	h_log::cout << "Angle for shortest: " << angle / M_PI << std::endl;
-	if(std::abs(angle) < M_PI / 4)
-		return EAST;
-	if(-angle < 3 * M_PI / 4 && -angle > M_PI / 4)
-		return NORTH;
-	if( angle < 3 * M_PI / 4 &&  angle > M_PI / 4)
-		return SOUTH;
-	return WEST;
-}
-
-hlt::Move makeMove(const hlt::Location& location, hlt::GameMap& map, unsigned char myID) {
-	unsigned char strategy = 0;
-	bool on_border = hasEnemies(location, map);
-	if(on_border){
-		int best_score = 0;
-		for(unsigned char direction = 1; direction <= 6; ++direction) {
-			hlt::Site destination = map.getSite(location, direction);
-			if(destination.owner != myID && destination.strength - map.getSite(location).strength < best_score) {
-				best_score = destination.strength - map.getSite(location).strength;
-				strategy = direction;
-			}
-		}
-		if( strategy != 0 ) {
-			return {location, strategy};
-		}
-	}
-
-	if(map.getSite(location).strength < 5 * map.getSite(location).production) {
-		return {location, strategy};
-	}
-
-	if(!on_border) {
-		return {location, findShortestPathToEnemy(location, map)};
-	}
-
-	return { location , strategy };
-}
-
-
 int main() {
     srand(time(NULL));
-
-	h_log::cout.open("game.log");
-	h_log::cout << " ===== STARTING GAME =====" << std::endl;
 
     std::cout.sync_with_stdio(0);
 
@@ -114,7 +28,7 @@ int main() {
         for(unsigned short a = 0; a < presentMap.height; a++) {
             for(unsigned short b = 0; b < presentMap.width; b++) {
                 if (presentMap.getSite({ b, a }).owner == myID) {
-                    moves.insert(makeMove({ b, a },  presentMap, myID));
+                    moves.insert({ { b, a }, (unsigned char)(rand() % 5) });
                 }
             }
         }
@@ -122,6 +36,5 @@ int main() {
         sendFrame(moves);
     }
 
-	h_log::cout.close();
     return 0;
 }
--- Halite-C++-Starter-Package/runGame.sh
+++ Halite-C++-Starter-Package/runGame.sh
@@ -2,9 +2,6 @@
 
 mkdir -p replays
 mv *.hlt replays/
-rm game.log
-touch game.log
-rm *.o
 g++ -std=c++11 MyBot.cpp -o MyBot.o
 g++ -std=c++11 RandomBot.cpp -o RandomBot.o
 ./halite -d "30 30" "./MyBot.o" "./RandomBot.o"
